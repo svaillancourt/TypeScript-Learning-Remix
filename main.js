324b86a2ee484844ccda67d8048d444fb1740354
Stephane Vaillancourt <svaillancourt@gmail.com>
2020-04-01 15:00:24 -0600

    remix

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("This is a website about dogs at a distance!");
+console.log("It's because i can't get close to dogs due to my allergies :(");
+console.log("This is the website i'm refering to https://www.instagram.com/dogsatadistance/?hl=en");
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,3 @@
+console.log("This is a website about dogs at a distance!" )
+console.log("It's because i can't get close to dogs due to my allergies :(")
+console.log("This is the website i'm refering to https://www.instagram.com/dogsatadistance/?hl=en")
